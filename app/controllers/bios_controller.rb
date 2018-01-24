f87716f06323007a775680cc30f6d93bb033776f
kwin555 <ktn555@att.net>
2018-01-24 18:14:51 +0000

    v9 linked submit button to database

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -6,4 +6,15 @@ class BiosController < ApplicationController
   def new
     @bio = Bio.new
   end
+
+  def create
+    Quote.create(bio_parmas)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:bio).permit(:person, :life, :dob, :hobbies)
+  end
 end
